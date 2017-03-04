4464691d7322091f29b109431e2821a2187217a5
generalwrex <bjdroot@gmail.com>
2017-03-04 01:36:30 -0800

    Some work on the ini loader made the buttons do button things! It currently loads a GameServer.ini into the properties control, no saving yet!

---
+++ HellionServerManager/Config.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HellionServerManager
+{
+    public partial class Config : Form
+    {
+        public Config()
+        {
+            InitializeComponent();
+
+            textBox1.Text = Properties.Settings.Default.HellionDedicatedPath;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            Properties.Settings.Default.HellionDedicatedPath = folderBrowserDialog1.SelectedPath;
+            textBox1.Text = folderBrowserDialog1.SelectedPath;
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!System.IO.Directory.Exists(Properties.Settings.Default.HellionDedicatedPath))
+            {
+                MessageBox.Show("The selected Path is not valid!", "Invalid Path", MessageBoxButtons.OK);
+                button1_Click(this, null);
+
+            }
+            else
+            {
+                Properties.Settings.Default.Save();
+                this.Visible = false;              
+            }
+            
+
+            
+        }
+    }
+}
--- HellionServerManager/Form1.cs
+++ HellionServerManager/Form1.cs
@@ -1,65 +1,101 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using HellionServerManager.Controls;
 
 namespace HellionServerManager
 {
     public partial class Form1 : Form
     {
+        private GameServerIni m_gameServerIni;
+        private Form m_config;
+
         public Form1()
         {
             InitializeComponent();
 
+            m_config = new Config();
+            m_config.Visible = false;
+
+            m_gameServerIni = new GameServerIni();
+            m_gameServerIni.LoadDefaults();
+            m_gameServerIni.ServerPath = Properties.Settings.Default.HellionDedicatedPath;
+
+            ServerConfig_PropertyGrid.ToolbarVisible = false;
+            ServerConfig_PropertyGrid.SelectedObject = m_gameServerIni;
+
+            if (Properties.Settings.Default.FirstRun && Properties.Settings.Default.HellionDedicatedPath == String.Empty)
+            {
+                m_config.ShowDialog();
+                             
+                Properties.Settings.Default.FirstRun = false;
+                //Properties.Settings.Default.Save();
+            }
+
+
+
             
+            
+
         }
+
 
         #region File Menu
         private void ToolBar_File_Update_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show("Not Implemented Yet!","Not Implemented", MessageBoxButtons.OK);
         }
 
         private void ToolBar_Options_HSMConfig_Click(object sender, EventArgs e)
         {
-
+            m_config.ShowDialog();
         }
 
         private void ToolBar_File_Exit_Click(object sender, EventArgs e)
         {
-
+            Application.Exit();
         }
 
         private void ToolBar_Help_About_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(" Im the about messagebox!", "Fill me in!", MessageBoxButtons.OK );
+            MessageBox.Show(" I'm the about message box!", "Fill me in!", MessageBoxButtons.OK );
         }
         #endregion
 
 
         #region Server Config
 
+        private void GenerateProperties()
+        {
+            ServerConfig_PropertyGrid.Container.Add(new Component() { });
+        }
+
+
         private void Config_Button_Reload_Click(object sender, EventArgs e)
         {
-
+            m_gameServerIni.Load();
         }
 
         private void Config_Button_Save_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show("Not Implemented Yet!", "Not Implemented", MessageBoxButtons.OK);
         }
 
         private void Config_Button_Cancel_Click(object sender, EventArgs e)
         {
-
+            m_gameServerIni.Load();
+            ServerConfig_PropertyGrid.Refresh();
         }
+
 
         #endregion
 
+        private void ServerConfig_LoadDefaults_Click(object sender, EventArgs e)
+        {
+            m_gameServerIni.LoadDefaults();
+            ServerConfig_PropertyGrid.Refresh();
+        }
     }
 }
