65fb94f5130beea395b65e64150e95657e43b828
generalwrex <bjdroot@gmail.com>
2017-03-03 17:46:34 -0800

    initial project upload

---
+++ HellionServerManager/Debug.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HellionServerManager
+{
+    public static class Log
+    {
+
+        public static Boolean DebugMode = true;
+
+        public static void Message(string line)
+        {
+            if (DebugMode)
+                Console.WriteLine(line);
+            else
+                MessageBox.Show(line);
+
+        }
+
+    }
+}
---
+++ HellionServerManager/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace HellionServerManager
+{
+    static class Program
+    {
+        public static Thread UIThread;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            UIThread = new Thread(() => {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            });
+            UIThread.SetApartmentState(ApartmentState.STA);
+            UIThread.Start();
+
+            Log.DebugMode = true;
+
+        }
+    }
+}
